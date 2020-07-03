a0f859d98c53ea46e17a66a848a09c17576037c0
Stu Brown <stub2001@hotmail.com>
2020-07-03 10:55:36 +0100

    add v1 cleanup

--- build/js/temp/custom.js
+++ build/js/temp/custom.js
@@ -107,32 +107,7 @@ $(document).ready(function(){
 	});
 
 
-	/*----------------------------------------------------*/
-	/*  Back to Top
-	/*----------------------------------------------------*/
 
-	// Button
-	function backToTop() {
-		$('body').append('<div id="backtotop"><a href="#"></a></div>');
-	}
-	backToTop();
-
-	// Showing Button
-	var pxShow = 600; // height on which the button will show
-	var scrollSpeed = 500; // how slow / fast you want the button to scroll to top.
-
-	$(window).scroll(function(){
-	 if($(window).scrollTop() >= pxShow){
-		$("#backtotop").addClass('visible');
-	 } else {
-		$("#backtotop").removeClass('visible');
-	 }
-	});
-
-	$('#backtotop a').on('click', function(){
-	 $('html, body').animate({scrollTop:0}, scrollSpeed);
-	 return false;
-	});
 	
 
 	/*--------------------------------------------------*/
